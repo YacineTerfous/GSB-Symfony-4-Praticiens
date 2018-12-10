e39f55e85aafaa3a16d4a9291df935c8b46c6e7a
Erwan Guibert <32639781+Xissamine@users.noreply.github.com>
2018-12-10 16:02:59 +0100

    Ajout fonction Add pour praticien

--- GSB_praticien/src/Controller/PraticienController.php
+++ GSB_praticien/src/Controller/PraticienController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Praticien;
 use App\Repository\PraticienRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,5 +25,35 @@ class PraticienController extends AbstractController
         return $this->render('praticien/index.html.twig', [
             'praticiens'=>$praticiens
         ]);
+    }
+
+    /**
+     * @Route("/praticien/new", name="praticien_create")
+     */
+    public function form_pra(Praticien $praticien = null, Request $request,
+    ObjectManager $manager)
+    {
+        if(!$praticien)
+        {
+            $praticien = new Praticien();
+        }
+        $form = $this->createFormBuilder($praticien)
+                     ->add('pra_nom')
+                     ->add('pra_adr')
+                     ->add('pra_CP')
+                     ->add('pra_ville')
+                     ->add('pra_coefnotoriete')
+                     ->getForm();
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($praticien);
+            $manager->flush();
+            
+            return $this->redirectToRoute('praticien');
+        }
+        
     }
 }
--- GSB_praticien/src/Migrations/Version20181210133754.php
+++ GSB_praticien/src/Migrations/Version20181210133754.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181210080939 extends AbstractMigration
+final class Version20181210133754 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
