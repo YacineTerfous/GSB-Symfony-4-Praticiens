8772b9ba69a37122100a5729dcdac73ae34d79b2
YacineTerfous <44774858+YacineTerfous@users.noreply.github.com>
2019-01-08 16:43:08 +0100

    kj

--- GSB_praticien/src/Controller/SecurityControllerController.php
+++ GSB_praticien/src/Controller/SecurityControllerController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\User;
 use App\Form\RegistrationType;
 
